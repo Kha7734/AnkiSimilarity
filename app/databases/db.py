80d053bde5b385a38d02c0c600a79c4adeba5556
Kha7734 <huynhsikha2003@gmail.com>
2024-12-01 21:13:12 +0700

    - Update config.json

--- app/databases/db.py
+++ app/databases/db.py
@@ -11,10 +11,6 @@ with open('config.json', 'r') as config_file:
 # Replace <db_password> with the actual password
 config['mongodb']['uri'] = config['mongodb']['uri'].replace('<db_password>', constant.MONGODB_PASSWORD)
 
-# Save the updated config back to config.json
-with open('config.json', 'w') as config_file:
-    json.dump(config, config_file, indent=4)
-
 # Connect to MongoDB using the updated config
 client = MongoClient(config['mongodb']['uri'])
 db = client[config['mongodb']['database']]
